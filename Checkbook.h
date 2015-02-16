29f766fcfccf6844878d44ecd894d69b4f239336
Minos Park <me@minos.cc>
2015-02-16 06:14:57 -0800

    Done Lab4b

--- Checkbook.h
+++ Checkbook.h
@@ -2,3 +2,66 @@
 // Programmer: Minos Park 
 // Editor(s) used: Sublime Text 2 
 // Compiler(s) used: G++
+
+#ifndef Checkbook_h
+#define Checkbook_h
+
+template <class DataType>
+class Checkbook
+{
+  DataType* data;
+  int* track;
+  DataType bal;
+  int size = 2, track_num = 0;
+  void doubleArray();
+
+public:
+  Checkbook(const DataType&);
+  DataType* getChecks() const;
+  DataType getBalance() const {return bal;}
+  void writeChecks(const DataType&);
+  int getNumCheckWritten() const {return track_num;}
+  void makeDeposit(const DataType& deposit){bal += deposit;}
+};
+
+template <class DataType>
+Checkbook<DataType>::Checkbook(const DataType& initialBal)
+{
+  data = new DataType[size];
+  track = new int[size]();
+  bal = initialBal;
+}
+template <class DataType>
+DataType* Checkbook<DataType>::getChecks() const
+{
+  DataType* dumb = new DataType[size];
+  for (int i = 0; i < size; i++)
+    dumb [i] = data [i];
+  return dumb; // client must free the returned array after use
+}
+template <class DataType>
+void Checkbook<DataType>::writeChecks(const DataType& input)
+{
+  if(track[size - 1])  doubleArray();
+  bal -= input;
+  data[track_num] = input;
+  track[track_num] = 1;
+  track_num++;
+}
+template <class DataType>
+void Checkbook<DataType>::doubleArray()
+{
+  size *= 2;
+  DataType* temp = new DataType[size];
+  for (int i = 0; i < size/2; i++)
+    temp[i] = data[i];
+  delete [] data;
+  data = temp;
+
+  int* temp2 = new int[size]();
+  for (int i = 0; i < size/2; i++)
+  	temp2[i] = track[i];
+  delete [] track;
+  track = temp2;
+}
+#endif
--- MyCheckbook.cpp
+++ MyCheckbook.cpp
@@ -1,4 +1,109 @@
-// Lab 4a, Introduction To Dynamic Array Doubling And Deallocation
+// Lab 4b, Introduction To Dynamic Array Doubling And Deallocation
 // Programmer: Minos Park 
 // Editor(s) used: Sublime Text 2 
 // Compiler(s) used: G++
+#include "Checkbook.h"
+#include <iostream>
+using namespace std;
+
+#define DataType double
+#define DataInput(...) atof(__VA_ARGS__)
+#define initialBalance 0;
+
+char buf[100];
+Checkbook <DataType> check(0);
+
+void writeCheck();
+void makeDeposit();
+void listCheck();
+
+int main() 
+{
+  cout << "Lab 4b, Introduction To Dynamic Array Doubling And Deallocation"; 
+  cout << "Programmer: Minos Park\n"; 
+  cout << "Editor(s) used: Sublime Text 2\n"; 
+  cout << "Compiler(s) used: G++\n"; 
+  cout << "File: " << __FILE__ << endl; 
+  cout << "Complied: " << __DATE__ << " at " << __TIME__ << endl << endl; 
+
+  int select;
+  while(1)
+  {
+    cout << "================Check==Book================" << endl;
+    cout << "1: Get Balance" << endl;
+    cout << "2: Write a Check" << endl;
+    cout << "3: Make a Deposit" << endl;
+    cout << "4: Quit" << endl;
+    cout << "5: Other option; List All Checks" << endl;
+    cout << "Your Choice: ";
+    cin >> buf;
+    select = atoi(buf);
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "===========================================" << endl << endl;
+
+    switch(select)
+    {
+      case 1:
+        cout << "Your Balance is:" << check.getBalance() << endl;
+        cout << "Press ENTER to continue..." << endl;
+        cin.get();
+        break;
+      case 2:
+        writeCheck();
+        break;
+      case 3:
+        makeDeposit();
+        break;
+      case 4:  return 0;
+      case 5:
+        listCheck();
+        break;
+    }
+  }
+}
+
+void writeCheck()
+{
+  cout << "Check amount: ";
+  cin >> buf;
+  DataType temp = DataInput(buf);
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+  check.writeChecks(temp);
+  cout << "Check written successfully!" << endl;
+  cout << "Press ENTER to continue..." << endl;
+  cin.get();
+}
+
+void makeDeposit()
+{
+  cout << "Deposit amount: ";
+  cin >> buf;
+  DataType temp = DataInput(buf);
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+  check.makeDeposit(temp);
+  cout << "Deposit successful!" << endl;
+  cout << "Press ENTER to continue..." << endl;
+  cin.get();
+}
+
+void listCheck()
+{
+  if (check.getNumCheckWritten()==0)
+  {
+  	cout << "No Checks Written" << endl;
+  	cout << "Press ENTER to continue..." << endl;
+    cin.get();
+  	return;
+  }
+  cout << "List of Checks Written" << endl;
+  DataType* list = check.getChecks();
+  for (int i = 0; i < check.getNumCheckWritten(); i++)
+  {
+  	cout << "Check #" << i + 1 << ": " << list[i] << endl;
+  }
+  delete [] list;
+  cout << "Press ENTER to continue..." << endl;
+  cin.get();
+}
