dfe2fc823a689a2a51a2b88a3f5408c5e3220b46
Minos Park <me@minos.cc>
2015-03-16 00:03:04 -0700

    6B done

--- DvcSchedule6b.cpp
+++ DvcSchedule6b.cpp
@@ -39,6 +39,7 @@ int main()
 
   vector<SubjectCode> subjectCodes;
   SubjectCode tmp;
+  Course tmp2;
   vector<string> dup(1, "");
   char *token;
   char buf[1000];
@@ -46,7 +47,7 @@ int main()
   const char* const tab = "\t";
   int progressbar = 0;
   int courseCount = 0;
-  bool check = 0;
+  bool check = false, check2 = false;
   
   ifstream fin;
   clock_t startTime = clock();
@@ -55,7 +56,11 @@ int main()
   getline(fin, line); //Eliminate first line
   
   cout << "Progress Bar";
-  
+
+  tmp.sectionCount = 1;
+  subjectCodes.push_back(tmp);
+  subjectCodes[0].sectionCount = 0;
+
   while(fin.good())
   {
     if(progressbar % 1000 == 0)
@@ -64,7 +69,8 @@ int main()
       cout.flush();
     }
     progressbar++;
-    check = false;
+    check = false; check2 = false;
+    tmp.course.clear();
     
     getline(fin, line);
     strcpy(buf, line.c_str());
@@ -73,12 +79,15 @@ int main()
     const string term(token = strtok(buf, tab));
     const string section(token = strtok(0, tab));
     const string course((token = strtok(0, tab))?token : "");
-    const string courseName(course.begin(), course.begin() + course.find('-'));
+    const string fullcourse(course.begin(), course.begin() + course.find('-'));
     
-    tmp.name = courseName;
+    tmp.name = fullcourse;
     tmp.section = section;
     tmp.term = term;
-  
+    tmp2.coursename = course;
+    tmp2.count = 1;
+    tmp.course.push_back(tmp2);
+
     for(int i = 0; i < dup.size(); i++)
     { 
       if((dup[i].compare(0, term.size(), term)) == 0)
@@ -97,18 +106,30 @@ int main()
       if(i + 1 == dup.size())
         dup.push_back((string) term + ',' + section);
     }
-    
+
     if(check == false)
     {
       for (int i = 1; i <= courseCount; i++)
       {
         if(subjectCodes[i].name.compare(tmp.name) == 0)
         {
-          subjectCodes[i].sectionCount ++;
+          for (int j = 0; j< subjectCodes[i].course.size(); j++)
+          {
+            if (subjectCodes[i].course[j].coursename.compare(tmp2.coursename) == 0)
+            {
+              subjectCodes[i].course[j].count++;
+              check = true;
+              subjectCodes[i].sectionCount++;
+              check2 = true;
+              break;
+            }
+          }
+          if (check2 == true) break;
+          subjectCodes[i].course.push_back(tmp2);
           check = true;
+          subjectCodes[i].sectionCount++;
           break;
         }
-
       }
     }
     
@@ -116,13 +137,12 @@ int main()
     {
       courseCount++;
       assert(courseCount < 200);
-      subjectCodes[courseCount] = tmp;
-      subjectCodes[courseCount].sectionCount = 1;
+      subjectCodes.push_back(tmp);
     }
   }
   cout << endl << endl;
   fin.close();
-  
+
   cout << "Total " << courseCount << " classes" << endl << endl;
   for(int i = 1; i <= courseCount; i++)
   {
@@ -144,8 +164,8 @@ int main()
   for(int i = 1; i <= courseCount; i++)
   {
     cout << subjectCodes[i].name << ", " << subjectCodes[i].sectionCount << " classes" << endl;
-//    for (int j = 1; j <= subjectCodes[i].sectionCount; j++ )
-//      cout << << endl;
+    for (int j = 0; j < subjectCodes[i].course.size(); j++ )
+      cout << "  " << subjectCodes[i].course[j].coursename << ", " << subjectCodes[i].course[j].count << " classes"<< endl;
   }
   cout << endl;
   cout << "Duplicates: " << subjectCodes[0].sectionCount << endl << endl;
